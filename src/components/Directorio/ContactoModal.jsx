8e90cd3be5954be7997d32d001e747c1992b43e4
ztmarcos <ztmarcos@gmail.com>
2025-05-30 14:12:28 -0600

    Remove CSS import and file to fix Railway build - will add styling later

--- src/components/Directorio/ContactoModal.jsx
+++ src/components/Directorio/ContactoModal.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './ContactoModal.css';
+// CSS import temporarily removed
 
 const ContactoModal = ({ contacto, onSave, onClose }) => {
   const [formData, setFormData] = useState({
